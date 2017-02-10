9144f928d454aa3a5ad8cdae8118cc1fe7fde695
Karata <vabc3h@gmail.com>
2017-02-10 23:22:11 +0800

    Add hyper.

--- examples/a.rs
+++ examples/a.rs
@@ -1,8 +1,19 @@
 extern crate raft;
 use raft::Node;
 
+extern crate hyper;
+use hyper::server::{Server, Request, Response};
+
 fn main() {
     let a = Node::new();
     println!("{:?}", a);
     a.cluster.broadcast();
+
+    fn hello(req: Request, res: Response) {
+        println!("{:?}",req.remote_addr);
+        println!("ins");
+    }
+
+    Server::http("0.0.0.0:1025").unwrap().handle(hello).unwrap();
+    println!("not");
 }
