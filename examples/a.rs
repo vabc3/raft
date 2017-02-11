5168227c88e9f98e4a493dd7587fe2c68e32304c
Karata <vabc3h@gmail.com>
2017-02-11 11:30:54 +0800

    Support parse input.

--- examples/a.rs
+++ examples/a.rs
@@ -4,17 +4,31 @@ use raft::Node;
 extern crate hyper;
 use hyper::server::{Server, Request, Response};
 
+use std::env;
+
 fn main() {
-    let node_config = "127.0.0.1:2001, 127.0.0.1:2002, 127.0.0.1:2003".to_owned();
-    let a = Node::new(0, node_config);
+    let mut node_id = 0;
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 2 {
+        node_id = match args[1].parse() {
+            Ok(n) => n,
+            Err(_) => {
+                println!("Input is not integer.");
+                return;
+            }
+        };
+    }
+
+    let node_config = "127.0.0.1:2000, 127.0.0.1:2001, 127.0.0.1:2002".to_owned();
+    let a = Node::new(node_id, node_config);
     println!("{:?}", a);
     a.cluster.broadcast();
 
-    fn hello(req: Request, res: Response) {
-        println!("{:?}",req.remote_addr);
-        println!("ins");
-    }
+    // fn hello(req: Request, res: Response) {
+    //     println!("{:?}",req.remote_addr);
+    //     println!("ins");
+    // }
 
-    Server::http("0.0.0.0:1025").unwrap().handle(hello).unwrap();
+    // Server::http("0.0.0.0:1025").unwrap().handle(hello).unwrap();
     println!("not");
 }
--- src/node.rs
+++ src/node.rs
@@ -8,27 +8,39 @@ enum Status {
 }
 
 #[derive(Debug)]
+struct Server {
+    address: String,
+}
+
+impl Server {}
+
+#[derive(Debug)]
 pub struct Node {
     pub id: NodeId,
     pub cluster: Cluster,
     status: Status,
+    server: Server,
 }
 
 impl Node {
     pub fn new(id: NodeId, node_config: String) -> Node {
         let mut cluster = Cluster::new();
         let mut index = 0;
+        let mut server_address: Option<String> = None;
         for address in Self::parse_clients_str(node_config) {
             if index != id {
                 cluster.add_client(Box::new(ClientA::new(index, address)));
+            } else {
+                server_address = Some(address);
             }
-            
+
             index += 1;
         }
         Node {
             id: id,
             cluster: cluster,
             status: Status::Follower,
+            server: Server { address: server_address.expect("server addr be cound.") },
         }
     }
 
